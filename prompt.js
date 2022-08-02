e913e5b0919c7d8c0283c28d991e5838da1b05cf
Leco Arrindell <79813285+lecoa@users.noreply.github.com>
2022-08-02 16:55:04 +0200

    Initial commit

---
+++ prompt.js
@@ -0,0 +1,20 @@
+function show_prompt(title, message, pribtnHide = false, pribtnLabel = "Close", secBtnLabel = '', secBtnAction = '') {
+    document.getElementById("modal-alert-title").innerHTML = title;
+    document.getElementById("modal-alert-content").innerHTML = message;
+    if(pribtnHide){
+        document.getElementById("modal-button-primary").style.display = "none";    
+    }else{
+        document.getElementById("modal-button-primary").style.display = "inline-block";    
+    }
+    document.getElementById("modal-button-primary").innerText = pribtnLabel;
+    if(secBtnLabel != '' && secBtnAction != ''){
+        document.getElementById("modal-button-secondary").innerText = secBtnLabel;
+        document.getElementById("modal-button-secondary").setAttribute('onclick', secBtnAction);
+        document.getElementById("modal-button-secondary").style.display = "inline-block";
+        document.getElementById("modal-button-primary").classList.add("default");
+    }else{
+        document.getElementById("modal-button-secondary").style.display = "none";
+        document.getElementById("modal-button-primary").classList.remove("default");
+    }
+    document.getElementById("modal-alert").style.display = "block";
+}
